f21ac3e3e67e483b68edd486759b44e2cb296310
Kimberly Narine <kimberlyonarine@gmail.com>
2014-05-21 22:44:00 -0400

    Animates grid

--- game.py
+++ game.py
@@ -1,15 +1,46 @@
 from tkinter import *
 from random import choice
 
-gameSize = 300
+ball, gameSize = None, 300
+
+def ballPosition():
+    x1, y1, x2, y2 = canvas.coords(ball)
+    return[(x1 + x2)/2, (y1 + y2)/2]
+
+def randomSpeed():
+    return choice([10, -10])
+    
+def outBounds(coord):
+    return 0 > coord or coord > gameSize-10
 
 def startGame():
     headline['text'] = 'Welcome!'
+    
+    global canvas, ball
+    xLeftCoord = choice(list(range(gameSize-10)))
+    yLeftCoord = choice(list(range(gameSize-10)))
+    if ball:
+        canvas.delete(ball)
+    ball = canvas.create_rectangle(xLeftCoord, yLeftCoord, xLeftCoord+10, yLeftCoord+10, fill='red' )
+    print(ballPosition())
+    
+    animate()
     
 def endGame():
     print('Bye Bye!')
     root.destroy()
 
+def animate():
+    global ball
+    xSpeed = randomSpeed()
+    ySpeed = randomSpeed()
+    while outBounds(ballPosition()[0]+xSpeed):
+        xSpeed = randomSpeed()
+    while outBounds(ballPosition()[1]+ySpeed):
+        ySpeed = randomSpeed()
+    canvas.move(ball, xSpeed, ySpeed)
+    ball = canvas.create_rectangle(canvas.coords(ball), fill=choice(['purple', 'blue', 'yellow', 'orange', 'red', 'green']))
+    root.after(100, animate)
 
 
 root = Tk()
